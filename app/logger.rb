93e5201200ec8f2861aaa1c58fa3c188e8edf170
ogeidix <diegogiorgini@gmail.com>
2013-03-13 02:29:03 -0700

    add levels to logger

--- app/logger.rb
+++ app/logger.rb
@@ -1,8 +1,9 @@
 module CollaborativeEditing
     class Logger
     
-    	def initialize(logfile_name)
+    	def initialize(logfile_name, levels)
     	   @logfilename = logfile_name
+         @level       = levels
     		
     	     # TODO: before doing anything, apply recovery from the existing log file
            # delete an existing log file from older revision
@@ -15,15 +16,18 @@ module CollaborativeEditing
     	end
 
       def debug(message)
+        return unless @level.include?('debug')
         log_to_stdout message, 'd'
       end
 
       def info(message)
+        return unless @level.include?('info')
         log_to_stdout message
       end
 
       def recovery(message)
-        log_to_file message
+        log_to_file message 
+        return unless @level.include?('recovery')
         log_to_stdout message, 'r'
       end
 
