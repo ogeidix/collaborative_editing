6e39162c77d86d2dd6c13a8b37127409df27e6a0
Tejas Patil <tejas.patil.cs@gmail.com>
2013-03-25 15:48:37 -0700

    minor change in while logging

--- app/document.rb
+++ app/document.rb
@@ -13,16 +13,16 @@ module CollaborativeEditing
 
         attr_reader :filename, :version, :rexml_doc, :history
         
-        def operations_since_checkpoint  # getter
-          @@operations_since_checkpoint
+        def operations_since_checkpoint  
+          @@operations_since_checkpoint  # getter
         end
 
-        def reset_operations_since_checkpoint   # reset
-          @@operations_since_checkpoint = 0
+        def reset_operations_since_checkpoint   
+          @@operations_since_checkpoint = 0   # reset
         end
 
-        def UPDATE_FREQUENCY # getter
-          @@UPDATE_FREQUENCY
+        def UPDATE_FREQUENCY
+          @@UPDATE_FREQUENCY    # getter
         end
         
         def initialize(filename)
@@ -38,7 +38,8 @@ module CollaborativeEditing
                 @rexml_doc = REXML::Document.new(File.new("data/" + filename))
             else 
                 # if the file already exists, then
-                # perform recovery using logs before returning the file to the user
+                # return it
+                                # perform recovery using logs before returning the file to the user
                 @rexml_doc = REXML::Document.new(File.new("data/" + filename))
                 dirty = false
                 @version = @rexml_doc[0][1].string.split[2].to_i
@@ -71,6 +72,7 @@ module CollaborativeEditing
                     checksum = Digest::MD5.hexdigest(@rexml_doc.to_s)
                     update_master(checksum, 0)
                 end
+
             end
         end
     
@@ -89,7 +91,7 @@ module CollaborativeEditing
             suffix = current_node.value[this_change.position.offset, current_node.value.length]
             interim = ""
             
-            if  this_change.is_a? Deletion
+            if this_change.is_a? Deletion
                 this_length = this_change.length
                 if this_change.direction.eql?('left')
                     prefix  = current_node.value[0, this_change.position.offset - this_length]
@@ -106,9 +108,9 @@ module CollaborativeEditing
             
             # increment the version
             @version += 1
-            checksum = Digest::MD5.hexdigest(@rexml_doc.to_s)
             
             if toLog == true
+                checksum = Digest::MD5.hexdigest(@rexml_doc.to_s)
                 secure_change_in_logs(checksum, this_change)
                 @@operations_since_checkpoint += 1
             end
@@ -117,25 +119,26 @@ module CollaborativeEditing
         # Bring the on-disk copy in sync with in-memory version of the file
         def update_master(checksum, lsn_increment = 1)
         
-            # update the meta information in the file
+            # update the meta-information in the file
             @rexml_doc[1][1].string = "version = " + @version.to_s
             @rexml_doc[1][3].string = "md5_checksum = " + checksum
             @rexml_doc[1][5].string = "lsn = " + (Application.logger.lsn.to_i + lsn_increment).to_s
             
-            # write the file to the disk
+            # write the in-memory version of the file to the disk
             File.open("data/" + filename, 'w') {|f| f.write(@rexml_doc) }
         end
         
-        def secure_change_in_logs(checksum, this_change)
-            # Create the log message for the operation performed
-            # Log structure is:
-            # <filename> <version> <checksum> <username> <node> <offset> <change type> <change meta>
-            #
-            # The <change meta> for insertion is just the data inserted.
-            # For deletion, we need to store the direction and length of chars deleted.
-            # NOTE: Currently we are not using checksum for any logic that
-            #       could be possibly used for checking consistency of disk
-            
+        # Create the log message for the operation performed and pass this 
+        # to the logger code which will write it to the log file
+        # Log structure is:
+        # <filename> <version> <checksum> <username> <node> <offset> <change type> <change meta>
+        #
+        # The <change meta> for insertion is just the data inserted.
+        # For deletion, we need to store the direction and length of chars deleted.
+        #
+        # NOTE: Currently we are not using checksum for any logic that
+        #       could be possibly used for checking consistency of disk
+        def secure_change_in_logs(checksum, this_change)            
             log  = @filename.to_s                   + Application.logger.DELIMITER
             log += @version.to_s                    + Application.logger.DELIMITER
             log += checksum                         + Application.logger.DELIMITER
@@ -144,14 +147,15 @@ module CollaborativeEditing
             log += this_change.position.offset.to_s + Application.logger.DELIMITER
 
             if this_change.is_a? Insertion
-                log += "insertion"      + Application.logger.DELIMITER
-                log += change.content   + Application.logger.DELIMITER
+                log += "insertion"             + Application.logger.DELIMITER
+                log += this_change.content     + Application.logger.DELIMITER
             elsif this_change.is_a? Deletion
-                log += "deletion"       + Application.logger.DELIMITER
-                log += change.direction + Application.logger.DELIMITER
-                log += change.length    + Application.logger.DELIMITER
+                log += "deletion"              + Application.logger.DELIMITER
+                log += this_change.direction   + Application.logger.DELIMITER
+                log += this_change.length.to_s + Application.logger.DELIMITER
             end
 
+            # invoke the logger function to log this to the log file
             Application.logger.recovery log
         end
     end
